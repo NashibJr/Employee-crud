3b4bcc60287529fa4d525471fc028f2f88e80020
NashibJr <nashibjr@gmail.com>
2025-07-17 19:18:04 +0300

    Feat: Added the employee entity

--- src/main/java/com/example/ctpseduction/demo/entity/EmployeeEntity.java
+++ src/main/java/com/example/ctpseduction/demo/entity/EmployeeEntity.java
@@ -1,2 +1,27 @@
 package com.example.ctpseduction.demo.entity;
 
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.*;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Employee {
+    @Field("Full name")
+    private String name;
+
+    @Email(message = "Enter a valid email address")
+    @Indexed(unique = true)
+    private String email;
+
+    @Size(min = 10, message = "Enter a valid phone number")
+    private String phone;
+
+    private String address;
+}
